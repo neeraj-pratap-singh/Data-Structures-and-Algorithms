fa3b31fe0fe339c4b89c3714bf41a250554aa3e3
neeraj-singh <71137350+neeraj-pratap-singh@users.noreply.github.com>
2023-08-26 14:59:04 +0530

    Update BubbleSort.py

--- SortingArray/BubbleSort.py
+++ SortingArray/BubbleSort.py
@@ -1,13 +1,15 @@
 # Time Complexity: O(n^2)
 
-def bubble_sort(arr):
+def bubbleSort(arr):
+    """ Sorts the input array using the Bubble Sort algorithm. """
     n = len(arr)
     for i in range(n):
         for j in range(0, n-i-1):
             if arr[j] > arr[j+1]:
+                # Swap elements
                 arr[j], arr[j+1] = arr[j+1], arr[j]
 
 # Example usage
 arr = [64, 34, 25, 12, 22, 11, 90]
-bubble_sort(arr)
+bubbleSort(arr)
 print("Sorted array:", arr)
