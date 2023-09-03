cd757867a2556700c214aa9cfeb7a991acde59e6
neeraj-singh <71137350+neeraj-pratap-singh@users.noreply.github.com>
2023-09-03 17:12:37 +0530

    Create Ques14-Array Slicing.py

---
+++ ExerciseQuestionSets/Ques14-Array Slicing.py
@@ -0,0 +1,9 @@
+# Given an array of integers, write Python code to extract a subarray containing elements from index 2 to 5 (inclusive). Print the subarray.
+# Given array of integers
+originalArray = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+
+# Extract subarray from index 2 to 5 inclusive
+subArray = originalArray[2:6]  # Note that the end index in slicing is exclusive, hence we use 6
+
+# Print the subarray
+print("Subarray from index 2 to 5:", subArray)
