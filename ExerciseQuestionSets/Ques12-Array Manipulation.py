2de0e32b7275176e56755964a1fd261bcc933ea5
neeraj-singh <71137350+neeraj-pratap-singh@users.noreply.github.com>
2023-09-03 17:11:04 +0530

    Array Initialization problem

---
+++ ExerciseQuestionSets/Ques12-Array Manipulation.py
@@ -0,0 +1,2 @@
+# Given an array of integers, write a Python function to find the maximum subarray sum (the sum of a contiguous subarray) using the Kadane's algorithm. Provide the start and end indices of the subarray as well
+#need to discuss with Shakul
---
+++ ExerciseQuestionSets/Ques13-Array Initialization.py
@@ -0,0 +1,10 @@
+# Create an empty Python list and populate it with integers from 1 to 10 using a loop. Print the resulting array.
+# Initialize an empty list
+integerArray = []
+
+# Populate the list with integers from 1 to 10
+for i in range(1, 11):
+    integerArray.append(i)
+
+# Print the resulting array
+print("Resulting array:", integerArray)
