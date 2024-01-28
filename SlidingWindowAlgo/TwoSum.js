698b48c6e6f8ffaf61f525486036d8d90a856454
neeraj-singh <71137350+neeraj-pratap-singh@users.noreply.github.com>
2024-01-28 14:13:20 +0530

    Update TwoSum.js

--- SlidingWindowAlgo/TwoSum.js
+++ SlidingWindowAlgo/TwoSum.js
@@ -2,19 +2,20 @@
 // - Problem Statement: Given an array of integers, find two numbers such that they add up to a specific target.
 
 function twoSum(input, target) {
+    let allSets = []
     let dataSets = {};
     for (let i = 0; i < input.length; i++) {
         let num = input[i];
         let remainingValue = target - num;
         if (remainingValue in dataSets) {
-            return [input[dataSets[remainingValue]], input[i]]; // returns the indices of the two numbers
+            allSets.push([input[dataSets[remainingValue]], input[i]]); // returns the values of the two numbers
         }
         dataSets[num] = i; // store the index of the current number
     }
-    return null; // return null if no pair is found
+    return allSets; // return null if no pair is found
 }
 
 
-  let input = [1, 4, 2, 2, 5, 7];
+  let input = [1, 4, 2, 3, 5, 7];
   let target = 5;
   console.log(twoSum(input, target));
